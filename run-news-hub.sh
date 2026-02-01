7332dc8a9708adea629800e4f0cbb8b1eee6ba04
Woonmok <qw5354@gmail.com>
2026-02-01 13:34:44 +0900

    google key

--- run-news-hub.sh
+++ run-news-hub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # News Hub 실행 스크립트
 
-export GOOGLE_API_KEY="AIzaSyCd8j3-sGl77ffjsBkwOEY15519Q219ooc"
+export GOOGLE_API_KEY="AIzaSyBTXs3p__VnyIpYPIY9u3SUJyaoHitnDWU"
 cd /Users/seunghoonoh/Desktop/wave-tree-news-hub
 /Users/seunghoonoh/Desktop/wave-tree-news-hub/.venv/bin/python news_hub.py
